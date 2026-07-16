cd57f9418ff7069cdaee1745509d09645279d058
Subhajit Das <das-rebel@users.noreply.github.com>
2026-07-17 01:25:53 +0530

    Deploy: warm beige + KB tooltip with /portfolio-timeline basePath

--- _next/static/3YlU1THV1HLEJGV3QKWZC/_buildManifest.js
+++ _next/static/3YlU1THV1HLEJGV3QKWZC/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-timeline//_next/:path+",
+        "destination": "/portfolio-timeline/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
